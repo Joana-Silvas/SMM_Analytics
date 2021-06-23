9807b6e68c5ecc03008f7ebdbe5800bb32cc4593
Junior Bertão <81366341+jrbert@users.noreply.github.com>
2021-06-23 16:37:02 -0300

    Update query_create.sql
    3 perguntas desenvolvidas.

--- query_create.sql
+++ query_create.sql
@@ -9,3 +9,25 @@ FROM (
 WHERE Estrelas > 1000
 ORDER BY Estrelas desc
 LIMIT 10;
+
+#Os 3 países com mais jogadores.
+
+SELECT `player`.`flag` AS `nacionalidade`, count(*) AS `count`
+FROM `player`
+GROUP BY `player`.`flag`
+ORDER BY `count`DESC 
+limit 3
+
+#Os países com mais jogadores.
+
+SELECT `player`.`flag` AS `Nacionalidade`, count(*) AS `count`
+FROM `player`
+GROUP BY `player`.`flag`
+ORDER BY `count`desc 
+
+#Quais jogadores tem os maiores records.
+
+SELECT DISTINCT timeRecord, Player 
+FROM records
+ORDER BY timeRecord DESC
+LIMIT 10;
